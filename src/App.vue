df7acbc60be280a1095adc893a10901b105b4751
Yosuke Hori <ysk8.hori@gmail.com>
2019-12-07 22:20:49 +0900

    vueファイルをTSクラス化してテストが成功するところまで

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -27,35 +23,21 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default Vue.extend({
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-});
+import { Vue, Component } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+@Component({ components: { HelloWorld } })
+export default class App extends Vue {}
 </script>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,14 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+import Vue from "vue";
+import Vuetify from "vuetify";
+Vue.use(Vuetify);
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
